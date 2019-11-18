395f177de4fd54b72b8ef005828766107bda6ed7
Petru Conduraru <petruconduraru@outlook.com>
2019-11-18 15:12:46 +0100

    1st version

--- script.py
+++ script.py
@@ -8,9 +8,35 @@ stars = []
 titles = []
 urlss = []
 
-pages_to_scrape = 1
+pages_to_scrape = 5
 
 # Increment trough the pages
 for i in range(1, pages_to_scrape + 1):
     url = "http://books.toscrape.com/catalogue/page-{}.html".format(i)
     pages.append(url)
+
+for item in pages:
+    page = requests.get(item)
+    soup = bs4(page.text, "html.parser")
+    # print(soup.prettify())
+    for i in soup.findAll("h3"):
+        ttl = i.getText()
+        titles.append(ttl)
+    for j in soup.findAll("p", class_="price_color"):
+        price = j.getText()
+        prices.append(price)
+    for s in soup.findAll("p", class_="star-rating"):
+        for k, v in s.attrs.items():
+            star = v[1]
+            stars.append(star)
+    divs = soup.findAll("div", class_="image_container")
+    for thumbs in divs:
+        tgs = thumbs.find("img", class_="thumbnail")
+        urls = "http://books.toscrape.com/" + str(tgs["src"])
+        newurls = urls.replace("../", "")
+        urlss.append(newurls)
+
+data = {"Title": titles, "Prices": prices, "Stars": stars}
+df = pd.DataFrame(data=data)
+df.index += 1
+print(df)
